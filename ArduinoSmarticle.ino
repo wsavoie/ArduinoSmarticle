8b9d160108d961a27916945c5155709f2f4e0845
Will Savoie <wsavoie@gatech.edu>
2016-01-25 09:57:57 -0500

    removed some old unnecessary code,  re-enabled stress sensing, and added light upon stress

--- ArduinoSmarticle.ino
+++ ArduinoSmarticle.ino
@@ -1,17 +1,21 @@
 #include <Servo.h>
+#include <avr/io.h>
+#include <avr035.h>
+//#include "Servo8Bit.cpp"
+#define F_CPU 8000000UL
 #define myabs(n) ((n) < 0 ? -(n) : (n))
 //pin definitions
-#define servo1 5 	//D9
-#define servo2 6 	//D10
-#define mic     A0       //D3
-#define stressPin A1	//A2
+#define servo1 5 	//
+#define servo2 6 	//
+#define mic     A6      //
+#define stressPin A7	//
 #define led 13   	//13 SCK
 
 #define stressMoveThresh 2
 
 
-// SoftwareServo S1;
-// SoftwareServo S2;
+//Servo8Bit S1;
+//Servo8Bit S2;
 Servo S1;
 Servo S2;
 uint8_t stress = 0;
@@ -29,23 +33,14 @@ int oldVal	= 0;
 int newVal	= 0;
 int diff 	= 0;
 int curr 	= 0;
-
-
+bool ledVal = false;
 void moveMotor(uint8_t pos);
 void currentRead(uint16_t meanCurrVal);
 void getRange(uint16_t ftVal);
 void light(bool a);
 void setup() {
-	// SoftwareServo
-	// S1.attach(servo1);
-	// S2.attach(servo2);
-	// S1.setMinimumPulse(600);
-	// S2.setMinimumPulse(600);
-	// S1.setMaximumPulse(2400);
-	// S2.setMaximumPulse(2400);
-	
-	S1.attach(servo1);
-	S2.attach(servo2);
+	S1.attach(servo1,600,2400);
+	S2.attach(servo2,600,2400);
 
 	pinMode(led,OUTPUT);
 	
@@ -57,7 +52,7 @@ void setup() {
 
 void loop() 
 {          
-	bool a = false;
+	ledVal=false;
 	int midPtCross = 0;
 	int meanCurr = 0;
         oldVal = 0;
@@ -73,8 +68,7 @@ void loop()
 		int bb= oldVal > micMean;
 		diff = (newVal > micMean) - (oldVal > micMean);
 		diff = aa - bb;
-		diff = myabs(diff);
-                //diff= abs(diff);        
+		diff = myabs(diff);        
 	
 		midPtCross = midPtCross + diff;
                 
@@ -82,20 +76,20 @@ void loop()
 	
 	//bitshift divide by sample, meancurr=meancurr/(2^samps)
 	meanCurr >>= samps;
-	//currentRead(meanCurr);
+	currentRead(meanCurr);
 	
-	//if(stressCount<stressMoveThresh || rangeType==6 || rangeType==7) //if previous moves were 6 or 7, continue without stress
-	//{
-		getRange(midPtCross);
-	//}
-	
-        //light(a);
+	if(stressCount<stressMoveThresh || rangeType==6 || rangeType==7) //if previous moves were 6 or 7, continue without stress
+	{
+	  getRange(midPtCross);                
+	}
+	light(ledVal);
+        
 	delay(del);
 }
 
 void currentRead(uint16_t meanCurrVal)
 {
-	if(meanCurrVal<10)//make magic number into meaningful value!!
+	if(meanCurrVal<5)//make magic number into meaningful value!!
 	{
 		stressCount = 0;
 	}
@@ -104,6 +98,7 @@ void currentRead(uint16_t meanCurrVal)
 		stressCount++;
 		if(stressCount>=stressMoveThresh && rangeType!=6 && rangeType!=7)
 		{
+                        ledVal=true;
 			(stress > 1 ? stress=0 : (stress++));
 			moveMotor(stress);
 		}	
@@ -116,7 +111,7 @@ void getRange(uint16_t ftVal)// at thresh = 50, thresh = 216-433-650
 {  
 	bool a = false;
 	rangeType=0;
-       
+       //range freq = [50hz*rangeType, 50hz*(rangeType+1)] 
 	if (ftVal < thresh*2)                            
 		rangeType = 1;
 	else if ((ftVal>=2*thresh) && (ftVal<3*thresh))//216-433
@@ -133,11 +128,11 @@ void getRange(uint16_t ftVal)// at thresh = 50, thresh = 216-433-650
 		rangeType = 7;
 	else
 		rangeType = 8;
-     if (rangeType==3)
-        {a = true;}
-        else
-        {a=false;}
-	light(a);
+//     if (rangeType==3)
+//        {a = true;}
+//        else
+//        {a=false;}
+//	light(a);
   if (rangeType != prevVal)
   { 
     prevVal = rangeType;
@@ -157,92 +152,6 @@ void getRange(uint16_t ftVal)// at thresh = 50, thresh = 216-433-650
 }
 void moveMotor(uint8_t pos) //break method into chunks to allow "multithreading"
 {
-	// switch (pos)
-	// { ///1500 = straight out, my 0 degrees servo's 90 degrees
-	// case 0:
-		// S1.write(1500); 
-		// S2.write(1500);
-		// S1.write(1500); 
-		// S2.write(1500);
-	  // break;
-	// case 1:
-		// S1.write(1500 - 300);
-		// S2.write(1500 + 300);
-		// break;
-	// case 2:
-		// S1.write(1500 + 300);
-		// S2.write(1500 - 300);
-	  // break;
-	// case 3:
-		// S1.write(1500 + 900);
-		// S2.write(1500 - 900);
-	  // break;
-	// case 4:
-		// S1.write(1500 - 900);
-		// S2.write(1500 + 900);
-	  // break;
-	// case 5:
-		// S1.write(1500 + 900);
-		// S2.write(1500 + 900);
-	  // break;
-	// case 6:
-		// S1.write(maxx * 10 + 600);
-		// S2.write((minn) * 10 + 600);
-		// delay(del);
-
-		// S1.write(maxx * 10 + 600);
-		// S2.write((maxx) * 10 + 600);
-		// delay(del);
-
-		// S1.write(minn * 10 + 600);
-		// S2.write((maxx) * 10 + 600);
-		// delay(del);
-
-		// S1.write(minn * 10 + 600);
-		// S2.write((minn) * 10 + 600);
-		// delay(del);
-		// break;
-	// case 7:
-		// //S1.write(maxx * 10 + 600);
-		// //S2.write((midd) * 10 + 600);
-		// //delay(del);
-		// //S1.write(midd * 10 + 600);
-		// //S2.write((maxx) * 10 + 600);
-		// //delay(del);
-		// //S1.write(minn * 10 + 600);
-		// //S2.write((midd) * 10 + 600);
-		// //delay(del);
-		// //S1.write(midd * 10 + 600);
-		// //S2.write((minn) * 10 + 600);
-		// //delay(del);
-		// //S1.write(maxx * 10 + 600);
-		// //S2.write((midd) * 10 + 600);
-
-
-		// S1.write(maxx * 10 + 600);
-		// S2.write((midd) * 10 + 600);
-		// delay(del*2);
-		// S1.write(midd * 10 + 600);
-		// S2.write((midd) * 10 + 600);
-		// delay(del*2);
-		// S1.write(midd * 10 + 600);
-		// S2.write((minn) * 10 + 600);
-		// delay(del*2);
-		// S1.write(maxx * 10 + 600);
-		// S2.write((minn) * 10 + 600);
-		// //S1.write(maxx * 10 + 600);
-		// //S2.write((midd) * 10 + 600);
-
-		// //delay(del/2);
-		// break;
-	// case 8:
-		// S1.write(1500 + 900);
-		// S2.write(1500 - 900);
-		// break;
-	// }
-	
-	
-	
 switch (pos)
 { ///1500 = straight out, my 0 degrees servo's 90 degrees
     case 0:
@@ -305,13 +214,13 @@ switch (pos)
     	  
           S1.writeMicroseconds(maxx * 10 + 600);
           S2.writeMicroseconds((midd) * 10 + 600);
-          _delay_ms(del*2);
+          _delay_ms(del);
           S1.writeMicroseconds(midd * 10 + 600);
           S2.writeMicroseconds((midd) * 10 + 600);
-          _delay_ms(del*2);
+          _delay_ms(del);
           S1.writeMicroseconds(midd * 10 + 600);
           S2.writeMicroseconds((minn) * 10 + 600);
-          _delay_ms(del*2);
+          _delay_ms(del);
           S1.writeMicroseconds(maxx * 10 + 600);
           S2.writeMicroseconds((minn) * 10 + 600);
           //S1.writeMicroseconds(maxx * 10 + 600);
