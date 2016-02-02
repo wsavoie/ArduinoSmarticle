03e752af1f10ef816e515af2bb40c8f88e00c4a2
Will Savoie <wsavoie@gatech.edu>
2016-02-02 10:31:18 -0500

    added spacers to back of case which make servos less mobile

--- ArduinoSmarticle.ino
+++ ArduinoSmarticle.ino
@@ -7,8 +7,8 @@
 //pin definitions
 #define servo1 5 	//
 #define servo2 6 	//
-#define mic     A6      //
-#define stressPin A7	//
+#define mic     A6      // CHANGE BACK TO a6
+#define stressPin A7	// CHANGE BACK TO a7
 #define led 13   	//13 SCK
 
 #define stressMoveThresh 2
