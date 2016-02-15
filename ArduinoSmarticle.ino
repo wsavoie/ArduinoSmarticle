6d62ae20c6341af1e5dd3205cb7821ba012d0fbf
Will Savoie <wsavoie@gatech.edu>
2016-02-15 15:43:56 -0500

    updated arduino software

--- ArduinoSmarticle.ino
+++ ArduinoSmarticle.ino
@@ -1,8 +1,10 @@
+//#include <MegaServo.h>
+
 #include <Servo.h>
 #include <avr/io.h>
 #include <avr035.h>
-//#include "Servo8Bit.cpp"
-#define F_CPU 8000000UL
+
+//#define F_CPU 8000000UL
 #define myabs(n) ((n) < 0 ? -(n) : (n))
 //pin definitions 
 #define servo1 5 	//
@@ -13,9 +15,6 @@
 
 #define stressMoveThresh 2
 
-
-//Servo8Bit S1;
-//Servo8Bit S2;
 Servo S1;
 Servo S2;
 uint8_t stress = 0;
@@ -24,7 +23,7 @@ uint16_t micMean = 512;
 uint16_t const del = 200;
 uint8_t stressCount = 0;
 uint16_t rangeType=0;
-uint8_t thresh = 25;
+double thresh = 12.5;
 uint8_t minn = 0; uint8_t maxx = 180; uint8_t midd = 90;
 uint8_t matchCount = 0;
 uint16_t prevVal = 0;
@@ -41,13 +40,11 @@ void light(bool a);
 void setup() {
 	S1.attach(servo1,600,2400);
 	S2.attach(servo2,600,2400);
-
+ 
 	pinMode(led,OUTPUT);
 	
 	pinMode(stress,INPUT);
 	pinMode(mic,INPUT);
-     //Serial.begin(9600);
-    // Serial.println("Ready");
 }
 
 void loop() 
@@ -73,7 +70,7 @@ void loop()
 		midPtCross = midPtCross + diff;
                 
 	}
-	
+  //Serial.print(midPtCross);    // prints a tab
 	//bitshift divide by sample, meancurr=meancurr/(2^samps)
 	meanCurr >>= samps;
 	currentRead(meanCurr);
@@ -136,6 +133,9 @@ void getRange(uint16_t ftVal)// at thresh = 50, thresh = 216-433-650
 //        else
 //        {a=false;}
 //	light(a);
+  //Serial.print(" ");    // prints a tab
+  //Serial.print(rangeType);
+ // Serial.println("");    // prints a tab
   if (rangeType != prevVal)
   { 
     prevVal = rangeType;
@@ -151,6 +151,7 @@ void getRange(uint16_t ftVal)// at thresh = 50, thresh = 216-433-650
     delay(del);
     return;
   }
+
   moveMotor(rangeType);
 }
 void moveMotor(uint8_t pos) //break method into chunks to allow "multithreading"
@@ -226,9 +227,9 @@ switch (pos)
           _delay_ms(del);
           S1.writeMicroseconds(maxx * 10 + 600);
           S2.writeMicroseconds((minn) * 10 + 600);
-          //_delay_ms(del/2);
-          //S1.writeMicroseconds(maxx * 10 + 600);
-          //S2.writeMicroseconds((midd) * 10 + 600);
+          _delay_ms(del/2);
+          S1.writeMicroseconds(maxx * 10 + 600);
+          S2.writeMicroseconds((midd) * 10 + 600);
     	  
           //_delay_ms(del/2);
           break;
