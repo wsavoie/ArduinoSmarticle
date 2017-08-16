1fcbb4a96eb9681e5656df0a8c0ca8166207e80f
PHYS33229\root <wsavoie@gatech.edu>
2017-08-16 09:22:43 -0400

    added obs videos to supersmarticles matlab data collection script

--- supersmarticleMatlab/takeOptiRun.m
+++ supersmarticleMatlab/takeOptiRun.m
@@ -1,15 +1,27 @@
 t=120; %seconds
 client = natnet();
 
+%keypresses for starting recording for OBS
+robot = java.awt.Robot;
+
+
 % Set the conenction type to the multicast of Optitrack and set the proper
-% local loopback IPs.
+% local loopback IPs./
 client.ConnectionType = 'Multicast';
-client.ClientIP = '127.0.0.1';
+client.ClientIP ='127.0.0.1';
 client.HostIP = '127.0.0.1';
 client.connect();
 client.stopRecord;
+%make sure to set OBS's hotkeys for record/stop record to f3/f4
+%respectively
+robot.keyPress(java.awt.event.KeyEvent.VK_F3);
+pause(.05);
 client.startRecord;
-
+robot.keyRelease(java.awt.event.KeyEvent.VK_F3)
 pause(t);
 client.stopRecord;
+robot.keyPress(java.awt.event.KeyEvent.VK_F4);
+pause(.1);
+robot.keyRelease(java.awt.event.KeyEvent.VK_F4);
+
 beep;
