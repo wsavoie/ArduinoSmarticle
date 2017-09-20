2601510a8c924879755e079f0042f6302166375b
PHYS33229\root <wsavoie@gatech.edu>
2017-09-20 13:38:29 -0400

    forgot to stage correct file in last commit

--- crawlingSmarticle/crawlingSmarticleAuto/matlabScripts/plotCrawlingData.m
+++ crawlingSmarticle/crawlingSmarticleAuto/matlabScripts/plotCrawlingData.m
@@ -3,7 +3,7 @@ close all;
 % load('D:\ChronoCode\chronoPkgs\Smarticles\matlabScripts\amoeba\smarticleExpVids\rmv3\movieInfo.mat');
 
 % fold=uigetdir('A:\2DSmartData\');
-fold=uigetdir('A:\2DSmartData\crawl\crawl1.25');
+fold=uigetdir('A:\2DSmartData\crawl');
 load(fullfile(fold,'movieInfo.mat'));
 figure(1)
 SPACE_UNITS = 'm';
@@ -13,10 +13,10 @@ fold
 %************************************************************
 %* Fig numbers:
 %* 1. calibrate world direction with DIR
-%* 2. plot gait size
+%* 2. plot gait size  or plot avg speed distance/time vs gait radii with std
+%* 3. distance per gait
 %************************************************************
-% showFigs=[21 22 23];
-showFigs=[1 2];
+showFigs=[3];
 
 %params we wish to plot
 DIR=[]; RAD=[]; V=[];
@@ -119,4 +119,72 @@ if(showFigs(showFigs==xx))
     figText(gcf,16);
     
 end
-%% 2 plot avg speed distance/time vs gait radii with std
+%% 3 avg speed vs position
+xx=3;
+if(showFigs(showFigs==xx))
+    figure(xx);
+    hold on;
+    %     first get number of gait radii used
+    
+    uRadD1=unique(allPars(D1,2));
+    uRadD2=unique(allPars(D2,2));
+    
+    %get 3 z heights
+    %bottom and top of same gait, then bottom of next gait
+    plot(usedMovs(1).t,usedMovs(1).z);
+    [x,y]=ginput(3);
+    dist=x(3)-x(1);
+    gBott=y(1); %bottom of gait height
+    gTop=y(3);  %top of gait height
+    
+    [pksT,locsT]=findpeaks(usedMovs(1).z,'MinPeakDistance',usedMovs(1).fps*.9*dist,'minpeakHeight',y(2)*.95);
+    [pksB,locsB]=findpeaks(-usedMovs(1).z,'MinPeakDistance',usedMovs(1).fps*.9*dist,'minpeakHeight',-y(1)*1.05);
+%     [pksT,locsT]=findpeaks(usedMovs(1).z,usedMovs(1).t,'MinPeakDistance',dist*.9,'minpeakHeight',y(2)*.95);
+%     [pksB,locsB]=findpeaks(-usedMovs(1).z,usedMovs(1).t,'MinPeakDistance',dist*.9,'minpeakHeight',-y(1)*1.1);
+    %         plot(usedMovs(1).t,usedMovs(1).z);
+    %         hold on;
+%     plot(locsB,-pksB,'o');
+%     plot(locsT,pksT,'o');
+
+    plot(usedMovs(1).t(locsT),usedMovs(1).z(locsT),'o');
+    plot(usedMovs(1).t(locsB),usedMovs(1).z(locsB),'o');
+    gaitzD1=cell(length(uRadD1),1);
+    gaitzD2=cell(length(uRadD2),1);
+    for i=1:length(uRadD1)
+        
+        idxs=find(allPars(:,1)==1&allPars(:,2)==uRadD1(i));
+        %         get indexes of particular gait radii
+        
+        gaitzD1{i}=cell(length(idxs),2);
+        for j=1:length(idxs)
+            [pksT,locsT]=findpeaks(usedMovs(idxs(j)).z,'MinPeakDistance',usedMovs(idxs(j)).fps*.9*dist,'minpeakHeight',y(2)*.95);
+            [pksB,locsB]=findpeaks(-usedMovs(idxs(j)).z,'MinPeakDistance',usedMovs(idxs(j)).fps*.9*dist,'minpeakHeight',-y(1)*1.05);
+%             [pksT,locsT]=findpeaks(usedMovs(idxs(j)).z,usedMovs(idxs(j)).t,'MinPeakDistance',dist*.9,'minpeakHeight',y(2)*.8);
+%             [pksB,locsB]=findpeaks(-usedMovs(idxs(j)).z,usedMovs(idxs(j)).t,'MinPeakWidth',dist*.9,'minpeakHeight',-y(1)*1.2);
+%             gaitzD1{i}{j,1}=[pksT,locsT];
+%             gaitzD1{i}{j,2}=[-pksB,locsB];
+            gaitzD1{i}{j,1}=[usedMovs(idxs(j)).x(locsT),diff(usedMovs(idxs(j)).x(locsT))];
+            gaitzD1{i}{j,2}=[usedMovs(idxs(j)).x(locsB),diff(usedMovs(idxs(j)).x(locsB))];
+        end
+    end
+    
+    for i=1:length(uRadD2) 
+        idxs=find(allPars(:,1)==2&allPars(:,2)==uRadD2(i));
+        gaitzD2{i}=cell(length(idxs),2);
+        for j=1:length(idxs)
+            [pksT,locsT]=findpeaks(usedMovs(idxs(j)).z,'MinPeakDistance',usedMovs(idxs(j)).fps*.9*dist,'minpeakHeight',y(2)*.95);
+            [pksB,locsB]=findpeaks(-usedMovs(idxs(j)).z,'MinPeakDistance',usedMovs(idxs(j)).fps*.9*dist,'minpeakHeight',-y(1)*1.05);
+%             [pksT,locsT]=findpeaks(usedMovs(idxs(j)).z,usedMovs(idxs(j)).t,'MinPeakDistance',dist*.9,'minpeakHeight',y(2)*.8);
+%             [pksB,locsB]=findpeaks(-usedMovs(idxs(j)).z,usedMovs(idxs(j)).t,'MinPeakDistance',dist*.9,'minpeakHeight',-y(1)*1.2);
+%             gaitzD2{i}{j,1}=[pksT,locsT];
+%             gaitzD2{i}{j,2}=[-pksB,locsB];
+            gaitzD2{i}{j,1}=[usedMovs(idxs(j)).x(locsT),diff(usedMovs(idxs(j)).x(locsT))];
+            gaitzD2{i}{j,2}=[usedMovs(idxs(j)).x(locsB),diff(usedMovs(idxs(j)).x(locsB))];
+        end
+    end
+    figText(gcf,16);
+    xlabel('gait num');
+    ylabel('displacement (mm)')
+end
+
+
