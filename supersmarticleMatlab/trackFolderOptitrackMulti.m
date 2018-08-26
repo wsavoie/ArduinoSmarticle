1b664c5422dc27bb42a5b9c6821f277061d40a14
PHYS33229\root <wsavoie@gatech.edu>
2018-08-26 17:33:22 -0400

    added supersmarticle tracking for ring and multi smarticles and a cloud program to fix rotation indices

---
+++ supersmarticleMatlab/trackFolderOptitrackMulti.m
@@ -0,0 +1,91 @@
+%clear all
+% fold=uigetdir('A:\2DSmartData');
+% fold=uigetdir('A:\2DSmartData\LightSystem\rossSmarts\mediumring');
+% fold=uigetdir('A:\2DSmartData\shortRing\redSmarts\');
+% fold=uigetdir('A:\2DSmartData\chordRing');
+fold=uigetdir('A:\2DSmartData\mediumRing\redSmarts\');
+f=dir2(fullfile(fold,'*.csv'));
+
+RIGIDBODYNAMES = true; %make true if tracking multiple things (i.e. inactive smarticles)
+clearvars rigidBodyName
+if RIGIDBODYNAMES
+    rigidBodyName{1} = ' ring';
+    rigidBodyName{2} = ' frame';
+    chordFlat=' flat';
+%     activeName= ' active';
+    inactiveName=' inactive';
+    frameName=' frame';
+        
+%     otherName=rigidBodyName{2};
+    otherName=rigidBodyName{2};
+numBods = 3;
+else
+    rigidBodyName = 'rigid body 1';
+end
+
+% r=.9525;
+%^N.B. put a space in front of keywords that could occur within other keywords
+%     clf;
+movs=struct;
+nMovs=length(f);
+movs(nMovs).fname='';
+r=.09525;%radius of boundary in meters
+dec=12; %decimate amount
+%HANDEDNESS IN QUATERNIONS ISNT CHANGED?
+conv=zeros(nMovs,1);
+
+closeWaitbar;
+fold
+h = waitbar(0,'Please wait...');
+steps = nMovs;
+idx=1;
+badIdx=0;
+failedAttempts=struct;
+if(exist('otherName','var'))
+    warning(['may want to change last argument in RIGIDBODYNAMES current name is ',otherName])
+end
+for i=1:nMovs
+    
+    % for i =1:length(f)
+    waitbar(i/steps,h,{['Processing: ',num2str(i),'/',num2str(length(f))],f(i).name})
+    %     pts(i,'/',nMovs);
+    %     [t,x,y,tracks]
+    
+    try
+        for jj=1:numBods
+            [movs(idx).t(:,jj),movs(idx).x(:,jj),movs(idx).y(:,jj),~,fps,movs(idx).rot(:,jj)]= trackOptitrack(fullfile(fold,f(i).name),dec,[' ',num2str(jj)]);
+        end
+        if exist('frameName','var')
+            %         [~,movs(idx).Ax,movs(idx).Ay,movs(idx).Adata,movs(idx).Arot]= trackOptitrack(fullfile(fold,f(i).name),dec,activeName);
+            [~,movs(idx).FRx,movs(idx).FRy,movs(idx).FRdata,movs(idx).fps, movs(idx).FRrot]= trackOptitrack(fullfile(fold,f(i).name),dec,frameName);
+        end
+        if exist('inactiveName','var')
+            %         [~,movs(idx).Ax,movs(idx).Ay,movs(idx).Adata,movs(idx).Arot]= trackOptitrack(fullfile(fold,f(i).name),dec,activeName);
+            [~,movs(idx).Ix,movs(idx).Iy,movs(idx).Idata,movs(idx).fps, movs(idx).Irot]= trackOptitrack(fullfile(fold,f(i).name),dec,inactiveName);
+        end
+        movs(idx).fname=f(i).name;
+        movs(idx).conv=1;
+        [~,vals]=parseFileNames(f(i).name);
+        vals=[0 0 1 5 i];
+        %     spk=[0]; smart=[-90]; gait=[1]; rob=[5]; v=[nMovs];
+        movs(idx).pars=vals;
+        idx=idx+1;
+    catch
+        badIdx=badIdx+1;
+        failedAttempts(badIdx).name=f(i).name;
+        
+    end
+end
+closeWaitbar;
+save(fullfile(fold,'movieInfo.mat'),'movs','fold','nMovs','r')
+if(badIdx>0)
+    pts(' ');
+    warning([num2str(badIdx),' failed runs']);
+    msg=cell(1,badIdx);
+    for i=1:badIdx
+        %         msg{yi}=failedAttempts(i).name;
+        pts(failedAttempts(i).name);
+    end
+    %     h=msgbox(msg,'errors');
+end
+beep;
